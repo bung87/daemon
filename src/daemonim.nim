02a1cab6ba5c58fa4ec6fcedb3da6843943bc2d3
Xabi Bello <xbello@gmail.com>
2020-06-27 15:05:39 +0200

    cherry pick pr

--- src/daemonim.nim
+++ src/daemonim.nim
@@ -5,247 +5,232 @@
 import os
 import posix
 import strutils
+import strformat
 
-const 
-    # defaultAppName = "daemonim"
-    DEVNULL = "/dev/null"
-    # VARRUN = "/var/run"
-    # STD_ERR_LOG = "$#-stderr.log" % defaultAppName
-    # STD_OUT_LOG = "$#-stdout.log" % defaultAppName
-    # STD_IN_LOG = "$#-stdin.log" % defaultAppName
-    # DEFAULT_PID_FILE =  "$#.pid" % defaultAppName
-    # TMP = "/tmp"
-    # defaultPidPath = when defined(macosx): TMP / DEFAULT_PID_FILE
-    # else: VARRUN / DEFAULT_PID_FILE
-    invalidPid = -1
+const
+  # defaultAppName = "daemonim"
+  DEVNULL = "/dev/null"
+  # VARRUN = "/var/run"
+  # STD_ERR_LOG = "$#-stderr.log" % defaultAppName
+  # STD_OUT_LOG = "$#-stdout.log" % defaultAppName
+  # STD_IN_LOG = "$#-stdin.log" % defaultAppName
+  # DEFAULT_PID_FILE =  "$#.pid" % defaultAppName
+  # TMP = "/tmp"
+  # defaultPidPath = when defined(macosx): TMP / DEFAULT_PID_FILE
+  # else: VARRUN / DEFAULT_PID_FILE
+  invalidPid* = -1
 
-type 
-    Daemon* = object of RootObj
-        pidfile:string
-        stdin:File
-        stdout:File
-        stderr:File
-        home_dir:string
-        umask:Mode
-        verbose:int
-        daemon_alive:bool
-        handler:proc() {.noconv.}
+type
+  Daemon* = object of RootObj
+    pidfile*: string
+    stdin: File
+    stdout: File
+    stderr: File
+    home_dir: string
+    umask: Mode
+    verbose: int
+    daemon_alive: bool
+    handler*: proc() {.noconv.}
 
-    DaemonRef* = ref Daemon
+  DaemonRef* = ref Daemon
 
 var glPidPath:string
 
-proc init(pidfile:string , 
-    stdin , stdout , stderr:File,
-    home_dir :string, umask:Mode, verbose:range[0..3]):Daemon{.noInit.} =
-    var 
-        result = Daemon()
-        pidpath = pidfile
-        # file:File
-    # try:
-    #     file = open(pidpath,fmReadWrite)
-    # except IOError:
-    #     if pidpath != defaultPidPath:
-    #         pidpath = defaultPidPath
-    #         try:
-    #             file = open(pidpath,fmReadWrite)
-    #         except IOError:
-    #             stderr.write(r"pidfile $# can't be opened \n" % [pidpath])
-    #             quit(1)
-    # defer: close(file)
-    glPidPath = pidpath
-    result.pidfile = pidpath
-    result.stdin = stdin
-    result.stdout = stdout
-    result.stderr = stderr
-    if home_dir.len > 0:
-        result.home_dir = home_dir
-        try:
-            setCurrentDir(home_dir)
-        except OSError:
-            discard
-    else:
-        result.home_dir = getCurrentDir()
-    result.umask = umask
-    result.verbose = verbose
-    result.daemon_alive = true
-    return result
+proc init(pidfile: string ,
+  stdin, stdout, stderr: File,
+  home_dir: string, umask: Mode, verbose: range[0..3]): Daemon {.noInit.} =
 
-proc initDaemon*(pidfile:string , 
-    stdin = stdin, stdout = stdout, stderr = stderr, 
-    home_dir = ".", umask:Mode = 0o22, verbose:range[0..3] = 0) : Daemon{.noInit.} = 
-    return init(pidfile , stdin , stdout , stderr,home_dir,umask,verbose)
+  result = Daemon()
+  var
+    pidpath = pidfile
 
-proc initDaemon*(pidfile:string , 
-    stdin, stdout, stderr:string, 
-    home_dir = ".", umask:Mode = 0o22, verbose:range[0..3] = 0) : Daemon{.noInit.} = 
-    discard system.stdin.reopen(stdin,fmRead)
-    discard system.stdout.reopen(stdout,fmAppend)
-    discard system.stderr.reopen(stderr,fmAppend)
-    return init(pidfile , system.stdin , system.stdout , system.stderr,home_dir,umask,verbose)
-# proc newDaemon[T](a:varargs[T]):DaemonRef = 
-#     new(result)
-#     for s in items(a):
-#         result[s] = s
-
-proc log(self:Daemon, args:varargs[string, `$`]) =
-    if self.verbose >= 1:
-        echo join(args)
-
-proc delpid(){.noconv.} =
-    var pid = invalidPid
+  glPidPath = pidpath
+  result.pidfile = pidpath
+  result.stdin = stdin
+  result.stdout = stdout
+  result.stderr = stderr
+  if home_dir.len > 0:
+    result.home_dir = home_dir
     try:
-        pid = parseInt(readFile(glPidPath).strip())
-    except OSError as e:
-        if errno == ENOENT:
-            discard
-        else:
-            raise
-    if pid == getpid():
-        removeFile(glPidPath)
+      setCurrentDir(home_dir)
+    except OSError:
+      discard
+  else:
+    result.home_dir = getCurrentDir()
+  result.umask = umask
+  result.verbose = verbose
+  result.daemon_alive = true
 
-template onQuit*(handler:proc(){.noconv, locks: 0.}) :typed =
-    bind glPidPath
-    addQuitProc(handler)
+proc `handler=`*(d: var Daemon, h: proc() {.noconv.}) =
+  d.handler = h
+
+proc initDaemon*(pidfile: string,
+  stdin = stdin, stdout = stdout, stderr = stderr,
+  home_dir = ".", umask: Mode = 0o22, verbose: range[0..3] = 0): Daemon {.noInit.} =
+  return init(pidfile, stdin, stdout, stderr, home_dir, umask, verbose)
+
+proc initDaemon*(pidfile: string ,
+  stdin, stdout, stderr: string,
+  home_dir = ".", umask: Mode = 0o22, verbose: range[0..3] = 0): Daemon {.noInit.} =
+  doAssert system.stdin.reopen(stdin, fmRead)
+  doAssert system.stdout.reopen(stdout, fmAppend)
+  doAssert system.stderr.reopen(stderr, fmAppend)
+  return init(pidfile, system.stdin, system.stdout ,system.stderr, home_dir, umask, verbose)
+
+proc log(self: Daemon, args: varargs[string, `$`]) =
+  if self.verbose >= 1:
+    self.stdout.writeLine join(args)
+    self.stdout.flushFile
+
+proc delpid(){.noconv, locks: 0.} =
+  var pid = invalidPid
+  try:
+    pid = parseInt(readFile(glPidPath).strip())
+  except IOError:
+    # Pid file didn't exist
+    discard
+  except OSError:
+    if errno == ENOENT:
+      discard
+    else:
+      raise
+  if pid == getpid():
+    removeFile(glPidPath)
+
+template onQuit*(handler: proc() {.noconv, locks: 0.}): typed =
+  bind glPidPath  # DEBUG: What for this capture?
+  addQuitProc(handler)
 
 proc daemonize(self: Daemon) =
-    var pid:Pid 
-    try:
-        pid = fork()
-    except  :
-        stderr.write("fork #1 failed: $# ($#)\n" % [$errno, getCurrentExceptionMsg()])
-        quit(1)
-    if pid > 0 :
-        # Exit first parent
-        quit(0)
-    # Decouple from parent environment
-    try:
-        discard chdir(self.home_dir)
-    except:
-        stderr.write("chdir failed: $# ($#)\n" % [$errno, getCurrentExceptionMsg()])
-        discard
-    discard setsid()
-    discard umask(self.umask)
+  var pid: Pid
+  try:
+    pid = fork()
+  except:
+    stderr.writeLine(&"fork #1 failed: {errno} ({getCurrentExceptionMsg()})")
+    quit(1)
+  if pid > 0:
+    # Exit first parent
+    quit(0)
+  # Decouple from parent environment
+  try:
+    discard chdir(self.home_dir)
+  except:
+    stderr.writeLine("chdir failed: {errno} ({getCurrentExceptionMsg})")
+  discard setsid()
+  discard umask(self.umask)
 
-    # Do second fork
-    try:
-        pid = fork()
-    except OSError :
-        stderr.write(
-            "fork #2 failed: $# ($#)\n" % [$errno, getCurrentExceptionMsg()])
-        quit(1)
-    if pid > 0:
-        # Exit from second parent
-        quit(0)
+  # Do second fork
+  try:
+    pid = fork()
+  except OSError :
+    stderr.writeLine(&"fork #2 failed: {errno} ({getCurrentExceptionMsg()})")
+    quit(1)
+  if pid > 0:
+    # Exit from second parent
+    quit(0)
 
-    self.stdout.flushFile()
-    self.stderr.flushFile()
+  self.stdout.flushFile()
+  self.stderr.flushFile()
 
-    # discard posix.dup2(getFileHandle(self.stdin),getFileHandle(stdin))
-    # discard posix.dup2(getFileHandle(self.stdout), getFileHandle(stdout))
-    # discard posix.dup2(getFileHandle(self.stderr), getFileHandle(stderr))
-    
-    onSignal(SIGTERM,SIGINT):
-        # self.daemon_alive = false
-        quit(0)
+  # discard posix.dup2(getFileHandle(self.stdin),getFileHandle(stdin))
+  # discard posix.dup2(getFileHandle(self.stdout), getFileHandle(stdout))
+  # discard posix.dup2(getFileHandle(self.stderr), getFileHandle(stderr))
 
-    self.log("Started")
-    
-    # onQuit():
-    #     var pid1:int = -1
-    #     try:
-    #         pid1 = parseInt(readFile(pidpath).strip())
-    #     except OSError as e:
-    #         if errno == ENOENT:
-    #             discard
-    #         else:
-    #             raise
-    #     if pid1 == getpid():
-    #         removeFile(pidpath)
-    # Write pidfile
-    # onQuit(mypidpath,handler)
-    onQuit(delpid)
-    # delpid()
-    # Make sure pid file is removed if we quit
+  onSignal(SIGTERM, SIGINT):
+    # self.daemon_alive = false
+    quit(0)
 
-    pid = getpid()
-    var pifile:File
-    defer:close(pifile)
-    pifile = open(self.pidfile, fmWrite)
-    pifile.write("$#\n" % $pid)
-    
+  self.log("Started")
 
-template daemonize*(self:Daemon, body: untyped) =
-    var handler:proc() {.noconv.} = proc () {.noconv.} = body
-    self.handler = handler
-    self.start()
+  # onQuit():
+  #   var pid1:int = -1
+  #   try:
+  #     pid1 = parseInt(readFile(pidpath).strip())
+  #   except OSError as e:
+  #     if errno == ENOENT:
+  #       discard
+  #     else:
+  #       raise
+  #   if pid1 == getpid():
+  #     removeFile(pidpath)
+  # Write pidfile
+  # onQuit(mypidpath,handler)
+  onQuit(delpid)
+  # delpid()
+  # Make sure pid file is removed if we quit
 
-proc start*(self:Daemon) =
-   
-    self.log("Starting...")
-    var mpid = invalidPid
-    # Check for a pidfile to see if the daemon already runs
-    try:
-        mpid = parseInt(readFile(self.pidfile).strip())
-    except IOError:
-        let message = "pidfile $# $#\n"
-        self.stderr.write(message % [self.pidfile, getCurrentExceptionMsg()])
-    except ValueError:
-        discard
-    # except SystemExit:
-    #     discard
+  let pifile: File = open(self.pidfile, fmWrite)
+  defer: close(pifile)
+  pifile.writeLine(&"{getpid()}")
 
-    if mpid != invalidPid:
-        let message = "pidfile $# already exists. Is it already running?\n"
-        self.stderr.write(message % [self.pidfile])
-        quit(1)
+template daemonize*(self: Daemon, body: untyped) =
+  var handler: proc() {.noconv.} = proc () {.noconv.} = body
+  self.handler = handler
+  self.start()
 
-    # Start the daemon
-    self.daemonize()
-    self.handler()
+proc start*(self: Daemon) =
+  self.log("Starting...")
+  var mpid = invalidPid
+  # Check for a pidfile to see if the daemon already runs
+  try:
+    mpid = parseInt(readFile(self.pidfile).strip())
+  except IOError:
+    self.stderr.writeLine(&"pidfile {self.pidfile} {getCurrentExceptionMsg()}")
+  except ValueError:
+    discard
+  # except SystemExit:
+  #   discard
 
-proc stop*(self:Daemon) =
-    ##[
-    Stop the daemon
-    ]##
-    var mpid:int = invalidPid
-    if self.verbose >= 1:
-        self.log("Stopping...")
+  if mpid != invalidPid:
+    self.stderr.writeLine(
+      &"pidfile {self.pidfile} already exists. Is it already running?")
+    quit(1)
 
-    # Get the pid from the pidfile
-    try:
-        mpid = parseInt(readFile(self.pidfile).strip())
-    except IOError:
-        discard
-    if mpid == invalidPid:
-        let message = "pidfile %s does not exist. Not running?\n"
-        self.stderr.write(message % self.pidfile)
+  # Start the daemon
+  self.daemonize()
+  self.handler()
 
-        # Just to be sure. A ValueError might occur if the PID file is
-        # empty but does actually exist
-        if existsFile(self.pidfile) :
-            removeFile(self.pidfile)
+proc stop*(self: Daemon) =
+  ##[
+  Stop the daemon
+  ]##
+  var mpid: int = invalidPid
+  if self.verbose >= 1:
+    self.log("Stopping...")
 
-        return  # Not an error in a restart
+  # Get the pid from the pidfile
+  try:
+    mpid = parseInt(readFile(self.pidfile).strip())
+  except IOError:
+    discard
+  if mpid == invalidPid:
+    self.stderr.writeLine(&"pidfile {self.pidfile} does not exist. Not running?")
 
-    # Try killing the daemon process
-    try:
-        var i = 0
-        while true:
-            discard kill(getPid(), SIGTERM)
-            sleep(6000)
-            i = i + 1
-            if i div 10 == 0:
-                discard kill(getPid(), SIGHUP)
-    except OSError as err:
-        if errno == ESRCH:
-            if existsFile(self.pidfile) :
-                removeFile(self.pidfile)
-        else:
-            echo repr(err)
-            quit(1)
+    # Just to be sure. A ValueError might occur if the PID file is
+    # empty but does actually exist
+    if existsFile(self.pidfile) :
+      removeFile(self.pidfile)
 
-    self.log("Stopped")
+    return  # Not an error in a restart
+
+  # Try killing the daemon process
+  try:
+    var i = 0
+    while true:
+      discard kill(getPid(), SIGTERM)
+      sleep(6000)
+      i = i + 1
+      if i div 10 == 0:
+        discard kill(getPid(), SIGHUP)
+  except OSError as err:
+    if errno == ESRCH:
+      if existsFile(self.pidfile) :
+        removeFile(self.pidfile)
+    else:
+      echo repr(err)
+      quit(1)
+
+  self.log("Stopped")
 
 proc restart*(self:Daemon) =
     ##[
@@ -254,38 +239,39 @@ proc restart*(self:Daemon) =
     self.stop()
     self.start()
 
-proc getPid*(self:Daemon):int =
-    var pid = invalidPid
-    try:
-        pid = parseInt(readFile(self.pidfile).strip())
-    except IOError:
-        discard
-    except ValueError:
-        discard
-    # except SystemExit:
-    #     pid = nil
-    return pid
+proc getPid*(self: Daemon): int =
+  var pid = invalidPid
+  try:
+    pid = parseInt(readFile(self.pidfile).strip())
+  except IOError:
+    discard
+  except ValueError:
+    discard
+  # except SystemExit:
+  #   pid = nil
+  return pid
+
 
 when defined(macosx): 
-    proc running(pid:int):bool =
-        try:
-            discard kill(Pid(pid),0)
-        except OSError:
-            return false
-        result = true
+  proc running(pid:int):bool =
+    try:
+      discard kill(Pid(pid),0)
+    except OSError:
+      return false
+    result = true
 else:
-    proc running(pid:int):bool =
-        result = existsFile("/proc/$#" % $pid)
+  proc running(pid:int):bool =
+    result = existsFile("/proc/$#" % $pid)
 
 proc is_running*(self:Daemon):bool =
-    let pid = self.getPid()
+  let pid = self.getPid()
 
-    if pid == invalidPid:
-        self.log("Process is stopped")
-        return false
-    elif running(pid): # mac has no /proc
-        self.log("Process (pid $#) is running..." % $pid)
-        return true
-    else:
-        self.log("Process (pid $#) is killed" % $pid)
-        return false
+  if pid == invalidPid:
+    self.log("Process is stopped")
+    return false
+  elif running(pid): # mac has no /proc
+    self.log("Process (pid $#) is running..." % $pid)
+    return true
+  else:
+    self.log("Process (pid $#) is killed" % $pid)
+    return false
